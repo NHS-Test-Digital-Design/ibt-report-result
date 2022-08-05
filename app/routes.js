63007706334e9e53e2cf544140098fdd111e450c
Russ Poulter <russell.poulter@infinityworks.com>
2022-08-05 12:13:04 +0100

    routing and linking

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+// report-result/test-for-work.html routing.
+router.post('/work-answer', function (req, res) {
+    var work = req.session.data['test-for-work']
+    if (work == "yes"){
+        res.redirect('report-result/about-work')
+    } else {
+        res.redirect('/report-result/test-reason')
+    }
+})
+
+// report-result/test-date.html routing.
+router.post('/date-answer', function (req, res) {
+    var date = req.session.data['date-taken']
+    if (date == "yesterday"){
+        res.redirect('report-result/barcode/control')
+    } else {
+        res.redirect('/report-result/barcode/challenger')
+    }
+})
 
 module.exports = router
