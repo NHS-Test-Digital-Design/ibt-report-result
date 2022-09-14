510fe8b19001ed8b5e7ad9e0ca5416905fcd204d
callumbuchanandesign <107179509+callumbuchanandesign@users.noreply.github.com>
2022-09-14 11:47:05 +0100

    Updates

--- app/routes.js
+++ app/routes.js
@@ -22,3 +22,20 @@ router.post('/date-answer', function (req, res) {
 })
 
 module.exports = router
+
+
+// Run this code when a form is submitted to 'country'
+router.post('/self-report/country', function (req, res) {
+    var country = req.session.data['country']
+    if (country == "england"){
+        res.redirect('self-report/england/test-for-work')
+    } else if (country == "scotland") {
+        res.redirect('order-lateral-flow-kits/scotland/eligibility-scotland')
+    } else if (country == "ni"){
+        res.redirect('order-lateral-flow-kits/ni/eligibility-ni')
+    } else if (country == "wales"){
+        res.redirect('order-lateral-flow-kits/wales/eligibility-wales')
+    } else {
+        res.redirect('order-lateral-flow-kits/condition')
+    }
+})
