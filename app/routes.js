ccbb68b8eb0754aab1b3d4f9c6f6a9bb3b422e8a
Russell Poulter <russell.poulter@infinityworks.com>
2023-03-28 10:44:36 +0100

    updated start page content and added took test for job route

--- app/routes.js
+++ app/routes.js
@@ -49,14 +49,78 @@ router.post('/country-answer', function (req, res) {
 
 
 
-router.post('/test-job', function (req, res) {
-    var date = req.session.data['test-for-job']
-    if (date == "whos-taking-the-test-1"){
-        res.redirect('/self-report/england/test-reason')
+router.post('/test-job-answer', function (req, res) {
+    var testJob = req.session.data['test-for-job']
+    if (testJob == "yes"){
+        res.redirect('/self-report/about-work')
     } else {
         res.redirect('/self-report/england/test-reason')
     }
 })
+
+router.post('/employer-provided-answer', function (req, res) {
+    var jobProvided = req.session.data['employer-provided']
+    if (jobProvided == "yes"){
+        res.redirect('/self-report/uon')
+    } else {
+        res.redirect('/self-report/england/test-date')
+    }
+})
+
+router.post('/about-work-answer', function (req, res) {
+    var aboutWork = req.session.data['about-the-work']
+    if (aboutWork == "social-care"){
+        res.redirect('/self-report/social-care-role')
+    } else if (aboutWork == "education"){
+        res.redirect('/self-report/education-provider')
+    } else if (aboutWork == "nhs"){
+        res.redirect('/self-report/nhs-setting')
+    } else {
+        res.redirect('/self-report/about-work-continued')
+    }
+})
+
+router.post('/work-continued-answer', function (req, res) {
+    var aboutWorkMore = req.session.data['about-the-work-continued']
+    if (aboutWorkMore == "ihp"){
+        res.redirect('/self-report/ihp-name')
+    } else if (aboutWorkMore == "homelessness"){
+        res.redirect('/self-report/uon')
+    } else if (aboutWorkMore == "domestic"){
+        res.redirect('/self-report/uon')
+    } else {
+        res.redirect('/self-report/employer-provided')
+    }
+})
+
+router.post('/social-care-role-answer', function (req, res) {
+    var careRole = req.session.data['social-care-role']
+    if (careRole == "workplce-tests"){
+        res.redirect('/self-report/social-care-id')
+    } else if (careRole == "visiting"){
+        res.redirect('/self-report/social-care-id')
+    } else if (careRole == "non-workplace-tests"){
+        res.redirect('/self-report/england/test-date')
+    } else {
+        res.redirect('/self-report/england/test-date')
+    }
+})
+
+router.post('/nhs-setting-answer-answer', function (req, res) {
+    var nhsSetting = req.session.data['nhs-setting']
+    if (nhsSetting == "hospital-trust" || 
+        nhsSetting == "ambulance-service-trust" ||
+        nhsSetting == "board-trust-wales" ||
+        nhsSetting == "community-trust" ||
+        nhsSetting == "community-company" ||
+        nhsSetting == "mental-health"
+        ){
+        res.redirect('/self-report/trust-name')
+    } else {
+        res.redirect('/self-report/work-postcode')
+    }
+})
+
 
 
 
